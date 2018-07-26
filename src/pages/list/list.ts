931227f921abb5ef3926e043f8914a986353bb2f
steeve <steeve.bois@gmail.com>
2018-07-26 14:34:08 +0200

    Ajout des sources de l'app

---
+++ src/pages/list/list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+//import { NavController, NavParams } from 'ionic-angular';
+//import { NativeAudio } from '@ionic-native/native-audio';
+//import { SplashScreen } from '@ionic-native/splash-screen';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+
+export class ListPage { 
+  
+}
